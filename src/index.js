39777d4adb89896084ce705e02fa2b5affa09385
rkahle <raphael.kahle@justtrade.com>
2022-06-11 18:20:21 +0200

    Added webpack boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* eslint-env browser */
+
+import someFunction from './some-function'
+
+
+console.log(someFunction('Jens'))
+
+const box = document.querySelector('#box')
+
+box.style.backgroundColor = 'blue'
---
+++ src/some-function.js
@@ -0,0 +1,1 @@
+export default function(name) { return `Hello ${name}!` }
